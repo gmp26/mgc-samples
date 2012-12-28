305858876abad411e35e848a65d7e5ce452a6e5b
gmp26 <gmp26@cam.ac.uk>
2012-12-28 21:52:41 +0000

    merged in mgc

--- mgc/modules/directives/stepper/stepper.js
+++ mgc/modules/directives/stepper/stepper.js
@@ -1,5 +1,11 @@
 angular.module('mgc.directives').directive('mgcStepper', ['mgc.config', function(mgcConfig) {
 	var options = mgcConfig.mgcStepper || {};
+	
+	var template = '<div class="input-prepend input-append">\
+	  <button class="btn icon-arrow-left" type="button"></button>\
+	  <input class="span2" id="appendedPrependedInput" type="number" value="{{amount}}">\
+	  <button class="btn icon-arrow-right" type="button"></button>\
+	</div>';
 	return {
 		restrict: 'A',
 		// supports using directive as an attribute, with options as the attribute value
@@ -8,7 +14,7 @@ angular.module('mgc.directives').directive('mgcStepper', ['mgc.config', function
 
 			// opts is link element-specific options merged on top of global defaults. 
 			// If you only extend the global default, then all instances would override each other
-			opts = angular.extend({}, options, attrs.mgcStepper);
+			opts = angular.extend({}, options, scope.$eval(attrs.mgcStepper));
  
 			// activate jQuery UI spinner 
 			var spinner = element.spinner(opts);
